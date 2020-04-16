78acd906b640b1c2ac25874a3e9a15c96d7185bd
Szczepan <l.szczepan@wp.pl>
2020-04-16 13:03:00 +0200

    basic app established

---
+++ app/src/main/java/com/example/forecastmvvm/ui/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.forecastmvvm.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.forecastmvvm.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        // NavigationUI.setupActionBarWithNavController(this, navController)
+        toolbar.setupWithNavController(navController)
+        bottom_nav.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+    }
+}
---
+++ app/src/main/java/com/example/forecastmvvm/ui/settings/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package com.example.forecastmvvm.ui.settings
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment() {
+
+//    PreferenceFragmentCompat()
+//    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+//        TODO("Not yet implemented")
+//    }
+}
